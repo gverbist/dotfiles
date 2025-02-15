e73d2d2db6785c2752fc42f4cb7dd9ab74a8a26b
Bist <bist@bist.be>
2025-02-15 10:19:20 +0100

    added vim autocmd for auto updating vimwiki

--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,18 +1,29 @@
 return {
 
-  "vimwiki/vimwiki",
-  lazy = false,
-  event = "BufEnter *.md",
-  keys = {"<leader>ww", "<leader>wt"},
-  init = function()
-      vim.g.vimwiki_folding = ""
-      vim.g.vimwiki_list = {
-            {
-                 path = "~/vimwiki/",
-                syntax = "markdown",
-                ext = ".md",
-            }
-            }
+	"vimwiki/vimwiki",
+	lazy = false,
+	event = "BufEnter *.md",
+	keys = { "<leader>ww", "<leader>wt" },
+	init = function()
+		vim.g.vimwiki_folding = ""
+		vim.g.vimwiki_list = {
+			{
+				path = "~/vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+			},
+		}
 
-        end,
+		-- Auto-push changes in Vimwiki when saving .wiki or .md files
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			pattern = { "*.wiki", "*.md" },
+			callback = function()
+				local home = os.getenv("HOME") or "~" -- Get home directory dynamically
+				local file_path = vim.fn.expand("%:p") -- Get full file path
+				if string.match(file_path, "^" .. home .. "/vimwiki/") then
+					vim.fn.jobstart({ "bash", home .. "/vimwiki/autopush.sh" }, { detach = true })
+				end
+			end,
+		})
+	end,
 }
